ea5e1b758a52119f909a778b27a3d32260df3d74
Robert Nelson <robertcnelson@gmail.com>
2017-11-02 11:35:19 -0500

    mirror is up
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/get_all.sh
+++ tools/get_all.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -e
 
 local_mirror="http://192.168.8.10/"
+web_mirror="http://rcn-ee.online/supercon/"
+
+dl_web () {
+	wget -c --directory-prefix=./${pre}/ ${web_mirror}${pre}/${file}
+}
+
+dl_local () {
+	wget --timeout=2 --tries=2 -c --directory-prefix=./${pre}/ ${local_mirror}${pre}/${file} || dl_web
+}
 
 if [ ! -d ./toolchain ] ; then
 	mkdir ./toolchain/
@@ -11,17 +20,25 @@ if [ ! -d ./rootfs ] ; then
 fi
 
 if [ ! -f ./toolchain/gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz ] ; then
-	wget -c --directory-prefix=./toolchain/ http://192.168.8.10/toolchain/gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz
+	pre="toolchain"
+	file="gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz"
+	dl_local
 fi
 
 if [ ! -f ./linux/linux-4.14-rc7.tar.gz ] ; then
-	wget -c --directory-prefix=./linux/ http://192.168.8.10/linux/linux-4.14-rc7.tar.gz
+	pre="linux"
+	file="linux-4.14-rc7.tar.gz"
+	dl_local
 fi
 
 if [ ! -f ./u-boot/u-boot-2017.11-rc3.tar.bz2 ] ; then
-	wget -c --directory-prefix=./u-boot/ http://192.168.8.10/u-boot/u-boot-2017.11-rc3.tar.bz2
+	pre="u-boot"
+	file="u-boot-2017.11-rc3.tar.bz2"
+	dl_local
 fi
 
 if [ ! -f ./rootfs/debian-9.2-iot-armhf-2017-11-02.tar.xz ] ; then
-	wget -c --directory-prefix=./rootfs/ http://192.168.8.10/rootfs/debian-9.2-iot-armhf-2017-11-02.tar.xz
+	pre="rootfs"
+	file="debian-9.2-iot-armhf-2017-11-02.tar.xz"
+	dl_local
 fi
