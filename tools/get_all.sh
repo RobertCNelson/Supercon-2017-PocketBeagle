0d2a11ad545bbc31e9f98e94e95984fc36cf8c27
Robert Nelson <robertcnelson@gmail.com>
2017-11-02 11:22:10 -0500

    add files
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/get_all.sh
+++ tools/get_all.sh
@@ -10,7 +10,18 @@ if [ ! -d ./rootfs ] ; then
 	mkdir ./rootfs/
 fi
 
-http://192.168.8.10/toolchain/gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz
-http://192.168.8.10/linux/linux-4.14-rc7.tar.gz
-http://192.168.8.10/u-boot/u-boot-2017.11-rc3.tar.bz2
-http://192.168.8.10/rootfs/debian-9.2-iot-armhf-2017-11-02.tar.xz
+if [ ! -f ./toolchain/gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz ] ; then
+	wget -c --directory-prefix=./toolchain/ http://192.168.8.10/toolchain/gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz
+fi
+
+if [ ! -f ./linux/linux-4.14-rc7.tar.gz ] ; then
+	wget -c --directory-prefix=./linux/ http://192.168.8.10/linux/linux-4.14-rc7.tar.gz
+fi
+
+if [ ! -f ./u-boot/u-boot-2017.11-rc3.tar.bz2 ] ; then
+	wget -c --directory-prefix=./u-boot/ http://192.168.8.10/u-boot/u-boot-2017.11-rc3.tar.bz2
+fi
+
+if [ ! -f ./rootfs/debian-9.2-iot-armhf-2017-11-02.tar.xz ] ; then
+	wget -c --directory-prefix=./rootfs/ http://192.168.8.10/rootfs/debian-9.2-iot-armhf-2017-11-02.tar.xz
+fi
