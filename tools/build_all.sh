8b62f606810b9a2a57a5b0a7cfcd067e17a34b9e
Robert Nelson <robertcnelson@gmail.com>
2017-11-02 12:09:32 -0500

    use more then one core
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/build_all.sh
+++ tools/build_all.sh
@@ -9,7 +9,7 @@ patch -p1 < ../0002-U-Boot-BeagleBone-Cape-Manager.patch
 
 make ARCH=arm CROSS_COMPILE=${CC} distclean
 make ARCH=arm CROSS_COMPILE=${CC} am335x_evm_defconfig
-make ARCH=arm CROSS_COMPILE=${CC}
+make -j3 ARCH=arm CROSS_COMPILE=${CC}
 cd ../../
 
 cd ./linux/linux-4.14-rc7/
@@ -17,5 +17,6 @@ patch -p1 < ../patch-4.14-rc7-bone3.diff
 make ARCH=arm CROSS_COMPILE=${CC} distclean
 cp -v ../defconfig ./.config
 #make ARCH=arm CROSS_COMPILE=${CC} menuconfig
-make ARCH=arm CROSS_COMPILE=${CC} zImage modules dtbs
+make -j3 ARCH=arm CROSS_COMPILE=${CC} zImage modules
+make ARCH=arm CROSS_COMPILE=${CC} dtbs
 cd ../../
