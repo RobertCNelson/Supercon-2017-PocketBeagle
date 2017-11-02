080e263247f433e056c6317187c6949222579e2d
Robert Nelson <robertcnelson@gmail.com>
2017-11-02 11:41:51 -0500

    extract files
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/get_all.sh
+++ tools/get_all.sh
@@ -42,3 +42,44 @@ if [ ! -f ./rootfs/debian-9.2-iot-armhf-2017-11-02.tar.xz ] ; then
 	file="debian-9.2-iot-armhf-2017-11-02.tar.xz"
 	dl_local
 fi
+
+if [ -f ./toolchain/gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf/gcc-linaro-6.4.1-2017.08-linux-manifest.txt ] ; then
+	rm -rf ./toolchain/gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf/ || true
+	echo "extracting: gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz"
+	cd ./toolchain/
+	tar xf gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz
+	cd ../
+else
+	echo "extracting: gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz"
+	cd ./toolchain/
+	tar xf gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz
+	cd ../
+fi
+
+if [ -f ./u-boot/u-boot-2017.11-rc3/Makefile ] ; then
+	rm -rf ./u-boot/u-boot-2017.11-rc3/ || true
+	echo "extracting: u-boot-2017.11-rc3.tar.bz2"
+	cd ./u-boot/
+	tar xf u-boot-2017.11-rc3.tar.bz2
+	cd ../
+else
+	echo "extracting: u-boot-2017.11-rc3.tar.bz2"
+	cd ./u-boot/
+	tar xf u-boot-2017.11-rc3.tar.bz2
+	cd ../
+fi
+
+if [ -f ./linux/linux-4.14-rc7/Makefile ] ; then
+	rm -rf ./linux/linux-4.14-rc7/ || true
+	echo "extracting: linux-4.14-rc7.tar.gz"
+	cd ./linux/
+	tar xf linux-4.14-rc7.tar.gz
+	cd ../
+else
+	echo "extracting: linux-4.14-rc7.tar.gz"
+	cd ./linux/
+	tar xf linux-4.14-rc7.tar.gz
+	cd ../
+fi
+
+
