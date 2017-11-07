0261e8769853d76ef2ce54303f0ad547dca29e61
Robert Nelson <robertcnelson@gmail.com>
2017-11-07 13:41:37 -0600

    merge these two
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/install_kernel.sh
+++ scripts/install_kernel.sh
@@ -2,6 +2,18 @@
 
 . system.sh
 
+kill_systemd_service () {
+	if [ -f /media/rootfs/lib/systemd/system/${service} ] ; then
+		sudo rm -f /media/rootfs/lib/systemd/system/${service}
+	fi
+}
+
+kill_system_service () {
+	if [ -f /media/rootfs/etc/init.d/${service} ] ; then
+		sudo rm -f /media/rootfs/etc/init.d/${service}
+	fi
+}
+
 if [ "x${MMC}" = "x" ] ; then
 	echo "Error, please set MMC in system.sh"
 	exit 1
@@ -38,47 +50,47 @@ if [ -f /media/rootfs/etc/fstab ] ; then
 		echo "Copying Modules"
 		sudo tar xf ./deploy/modules.tar.gz -C /media/rootfs/
 	fi
+
+	sync
+
+	service="alsa-restore.service" ; kill_systemd_service
+	service="alsa-state.service" ; kill_systemd_service
+	service="alsa-utils.service" ; kill_systemd_service
+	service="apache2.service" ; kill_systemd_service
+	service="apache2@.service" ; kill_systemd_service
+	service="apt-daily.service" ; kill_systemd_service
+	service="apt-daily.timer" ; kill_systemd_service
+	service="apt-daily-upgrade.service" ; kill_systemd_service
+	service="apt-daily-upgrade.timer" ; kill_systemd_service
+	service="avahi-daemon.service" ; kill_systemd_service
+	service="avahi-daemon.socket" ; kill_systemd_service
+	service="bb-wl18xx-bluetooth.service" ; kill_systemd_service
+	service="bb-wl18xx-wlan0.service" ; kill_systemd_service
+	service="beagle-tester.service" ; kill_systemd_service
+	service="bluetooth.service" ; kill_systemd_service
+	service="bonescript-autorun.service" ; kill_systemd_service
+	service="bonescript.service" ; kill_systemd_service
+	service="bonescript.socket" ; kill_systemd_service
+	service="capemgr.service" ; kill_systemd_service
+	service="cloud9.service" ; kill_systemd_service
+	service="cloud9.socket" ; kill_systemd_service
+	service="connman-wait-online.service" ; kill_systemd_service
+	service="node-red.service" ; kill_systemd_service
+	service="node-red.socket" ; kill_systemd_service
+	service="pppd-dns.service" ; kill_systemd_service
+	service="rc_battery_monitor.service" ; kill_systemd_service
+	service="roboticscape.service" ; kill_systemd_service
+	service="wpa_supplicant.service" ; kill_systemd_service
+	service="wpa_supplicant@.service" ; kill_systemd_service
+
+	service="alsa-utils" ; kill_system_service
+	service="apache2" ; kill_system_service
+	service="apache-htcacheclean" ; kill_system_service
+	service="avahi-daemon" ; kill_system_service
+	service="bluetooth" ; kill_system_service
+	service="cpufrequtils" ; kill_system_service
+	service="pppd-dns" ; kill_system_service
 fi
-
-sync
-
-service="alsa-restore.service" ; kill_systemd_service
-service="alsa-state.service" ; kill_systemd_service
-service="alsa-utils.service" ; kill_systemd_service
-service="apache2.service" ; kill_systemd_service
-service="apache2@.service" ; kill_systemd_service
-service="apt-daily.service" ; kill_systemd_service
-service="apt-daily.timer" ; kill_systemd_service
-service="apt-daily-upgrade.service" ; kill_systemd_service
-service="apt-daily-upgrade.timer" ; kill_systemd_service
-service="avahi-daemon.service" ; kill_systemd_service
-service="avahi-daemon.socket" ; kill_systemd_service
-service="bb-wl18xx-bluetooth.service" ; kill_systemd_service
-service="bb-wl18xx-wlan0.service" ; kill_systemd_service
-service="beagle-tester.service" ; kill_systemd_service
-service="bluetooth.service" ; kill_systemd_service
-service="bonescript-autorun.service" ; kill_systemd_service
-service="bonescript.service" ; kill_systemd_service
-service="bonescript.socket" ; kill_systemd_service
-service="capemgr.service" ; kill_systemd_service
-service="cloud9.service" ; kill_systemd_service
-service="cloud9.socket" ; kill_systemd_service
-service="connman-wait-online.service" ; kill_systemd_service
-service="node-red.service" ; kill_systemd_service
-service="node-red.socket" ; kill_systemd_service
-service="pppd-dns.service" ; kill_systemd_service
-service="rc_battery_monitor.service" ; kill_systemd_service
-service="roboticscape.service" ; kill_systemd_service
-service="wpa_supplicant.service" ; kill_systemd_service
-service="wpa_supplicant@.service" ; kill_systemd_service
-
-service="alsa-utils" ; kill_system_service
-service="apache2" ; kill_system_service
-service="apache-htcacheclean" ; kill_system_service
-service="avahi-daemon" ; kill_system_service
-service="bluetooth" ; kill_system_service
-service="cpufrequtils" ; kill_system_service
-service="pppd-dns" ; kill_system_service
 
 sync
 sudo umount /media/rootfs
