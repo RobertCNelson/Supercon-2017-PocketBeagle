991c418ef3cde250639b103a3b11f5da459dd846
Robert Nelson <robertcnelson@gmail.com>
2017-11-11 13:24:45 -0600

    Create 02_gpio.sh

---
+++ examples/02_gpio.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "GPIO59: config-pin -q P2_02"
+config-pin -q P2_02
+sleep 1
+echo "GPIO59: config-pin -l P2_02"
+config-pin -l P2_02
+sleep 1
+echo "GPIO59: config-pin P2_02 gpio_pu"
+config-pin P2_02 gpio_pu
+sleep 5
+echo "GPIO59: config-pin P2_02 gpio_pd"
+config-pin P2_02 gpio_pd
