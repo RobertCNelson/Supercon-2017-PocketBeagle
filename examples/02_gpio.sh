e51bb73a89e66ae5973f1074b8bfe03521dbad31
Lucas Rangit MAGASWERAN <lucasrangit@gmail.com>
2017-11-11 15:41:04 -0800

    examples: reset gpio to input in case it was set to output by another example

--- examples/02_gpio.sh
+++ examples/02_gpio.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "GPIO59: config-pin -q P2_02"
+echo "GPIO59: config-pin P2_02 in"
 config-pin -q P2_02
+sleep 1
+echo "GPIO59: config-pin -q P2_02"
 sleep 1
 echo "GPIO59: config-pin -l P2_02"
 config-pin -l P2_02
